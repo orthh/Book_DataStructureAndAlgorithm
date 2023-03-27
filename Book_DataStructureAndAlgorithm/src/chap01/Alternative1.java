421a8e4bd6997f98e0e8e7847f539d50ef04120e
playWitchicken <witchicken2@gmail.com>
2023-03-27 17:58:59 +0900

    refactor : remove if

--- Book_DataStructureAndAlgorithm/src/chap01/Alternative1.java
+++ Book_DataStructureAndAlgorithm/src/chap01/Alternative1.java
@@ -12,13 +12,10 @@ public class Alternative1 {
 		//두가지 문제
 		//1. 반복할때마다 if문 실행해야함
 		//2. 변경할때 유연하게 대응하기 어려움
-		for(int i=0; i<n; i++) {
-			if(i%2 == 0) {
-				System.out.print("+");
-			}else {
-				System.out.print("-");
-			}
+		for(int i=0; i<n/2; i++) {
+			System.out.println("+-");
 		}
+		if(n%2 != 0) System.out.println("+");
 	}
 
 }
